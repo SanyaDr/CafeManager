5f6d3d643f1c5f4f6fb6a1874f7ed8cba6a87d87
SanyaDr <112314312+SanyaDr@users.noreply.github.com>
2023-06-30 22:33:53 +0500

    final adjust side panel

---
+++ Models/FoodTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeManager3.Models
+{
+    public class FoodTypes
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
